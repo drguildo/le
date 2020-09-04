b5b7458cf7110aceb688b2bfe17bd0df8e1413f2
Simon Morgan <sjm@sjm.io>
2020-09-04 21:19:15 +0100

    Make use of is_empty

--- src/le.rs
+++ src/le.rs
@@ -12,11 +12,11 @@ pub struct LineEndingStats {
 
 impl LineEndingStats {
     pub fn is_lf(&self) -> bool {
-        self.lf.len() > 0 && self.crlf.len() == 0
+        self.lf.len() > 0 && self.crlf.is_empty()
     }
 
     pub fn is_crlf(&self) -> bool {
-        self.lf.len() == 0 && self.crlf.len() > 0
+        self.lf.is_empty() && self.crlf.len() > 0
     }
 
     pub fn is_mixed(&self) -> bool {
