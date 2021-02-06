97a6f049205610036d24461c7596347cab09ab55
Simon Morgan <sjm@sjm.io>
2021-02-06 18:59:54 +0000

    Move command line argument handling to its own function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
-use clap::{App, Arg};
+use clap::{App, Arg, ArgMatches};
 
 mod le;
+
+struct Configuration {
+    glob_pattern: Option<glob::Pattern>,
+    match_on: le::LineEndingType,
+    print_line_numbers: bool,
+}
 
 fn main() {
     let matches = App::new(clap::crate_name!())
@@ -37,36 +43,19 @@ fn main() {
         )
         .get_matches();
 
-    let match_on: le::LineEndingType = match matches.value_of("type").unwrap() {
-        "lf" => le::LineEndingType::LF,
-        "crlf" => le::LineEndingType::CRLF,
-        _ => le::LineEndingType::MIXED,
-    };
-
-    let print_line_numbers: bool = matches.is_present("line_numbers");
+    let config = get_configuration(&matches);
 
     for path in matches.values_of("paths").unwrap() {
-        if let Some(glob_string) = matches.value_of("glob_pattern") {
-            let glob_pattern =
-                glob::Pattern::new(glob_string).expect("Failed to read glob pattern");
-            process_path(path, Some(&glob_pattern), &match_on, print_line_numbers);
-        } else {
-            process_path(path, None, &match_on, print_line_numbers);
-        }
+        process_path(path, &config);
     }
 }
 
-fn process_path(
-    path: &str,
-    glob_pattern: Option<&glob::Pattern>,
-    match_on: &le::LineEndingType,
-    print_line_numbers: bool,
-) {
+fn process_path(path: &str, config: &Configuration) {
     for entry in walkdir::WalkDir::new(path) {
         match entry {
             Ok(entry) => {
                 if entry.file_type().is_file() {
-                    if let Some(glob_pattern) = glob_pattern {
+                    if let Some(glob_pattern) = config.glob_pattern.clone() {
                         if !glob_pattern.matches_path(entry.path()) {
                             continue;
                         }
@@ -77,7 +66,7 @@ fn process_path(
                         Ok(file_bytes) => {
                             let stats = le::count_line_endings(&file_bytes);
 
-                            match match_on {
+                            match config.match_on {
                                 le::LineEndingType::LF => {
                                     if stats.is_lf() {
                                         println!("{} has LF line endings", path_display);
@@ -91,7 +80,7 @@ fn process_path(
                                 le::LineEndingType::MIXED => {
                                     if stats.is_mixed() {
                                         println!("{} has mixed line endings", path_display);
-                                        if print_line_numbers {
+                                        if config.print_line_numbers {
                                             println!("LF: {:?}", stats.lf);
                                             println!("CRLF: {:?}", stats.crlf);
                                         }
@@ -108,4 +97,27 @@ fn process_path(
             Err(err) => eprintln!("{}", err),
         }
     }
+}
+
+fn get_configuration(matches: &ArgMatches) -> Configuration {
+    let mut config = Configuration {
+        glob_pattern: None,
+        match_on: le::LineEndingType::MIXED,
+        print_line_numbers: false,
+    };
+
+    config.match_on = match matches.value_of("type").unwrap() {
+        "lf" => le::LineEndingType::LF,
+        "crlf" => le::LineEndingType::CRLF,
+        _ => le::LineEndingType::MIXED,
+    };
+
+    config.print_line_numbers = matches.is_present("line_numbers");
+
+    if let Some(glob_string) = matches.value_of("glob_pattern") {
+        config.glob_pattern =
+            Some(glob::Pattern::new(glob_string).expect("Failed to read glob pattern"));
+    }
+
+    config
 }
